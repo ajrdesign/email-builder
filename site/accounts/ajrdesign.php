6a86760b7f881b1ed8ba7a4e1d0b8a6437f0f0f7
Andrew <ajrgraphicdesign@gmail.com>
2016-08-02 15:34:51 -0700

    removing gitignore from user folder

---
+++ site/accounts/ajrdesign.php
@@ -0,0 +1,14 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: ajrdesign
+password: >
+  $2a$10$MO3kyMJQILsAeG8U79lxUObZLctS2w1eyacFmQ/X0Tuf.lJ5WB8Je
+email: arichardson@bluebeam.ocm
+language: en
+role: admin
+history:
+  - email/hero-test
+  - templates/hero
+  - templates
+  - variety/july-2016
+  - templates/newsletter
