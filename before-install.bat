b1d027692a64a5f881ab54e54122e7674b603e2e
John Roh <jroh@bluebeam.com>
2016-09-18 14:30:20 -0700

    disable all in before-install.bat

--- before-install.bat
+++ before-install.bat
@@ -1,3 +1,3 @@
-Install Internet Information Server (IIS). 
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
+REM Install Internet Information Server (IIS). 
+REM c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
+REM c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
