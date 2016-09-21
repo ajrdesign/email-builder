04b2c7b771659aacdbf2912748e625fd82fe344a
Andrew <ajrgraphicdesign@gmail.com>
2016-09-21 10:03:21 -0700

    Test #4 with a twist

--- site/templates/home.php
+++ site/templates/home.php
@@ -5,7 +5,7 @@
 			<div class="col-md-12 center">
 				<h1 class="subheading"><?php echo $page->title()->html() ?></h1>
 			       <a href="panel/" class="btn btn-blue">Start Building</a>
-			       <p>Test #3</p>
+			       <p>Testing something entirely new now!</p>
 			</div>
 		</div>
 	</div>
