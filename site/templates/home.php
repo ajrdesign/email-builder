beef9fa7ad930c06e69fc8d0b5da22e6db4fa2b5
Andrew <ajrgraphicdesign@gmail.com>
2016-10-10 17:31:36 -0700

    Removing extra string from homepage

--- site/templates/home.php
+++ site/templates/home.php
@@ -5,7 +5,6 @@
 			<div class="col-md-12 center">
 				<h1 class="subheading"><?php echo $page->title()->html() ?></h1>
 			       <a href="panel/" class="btn btn-blue">Start Building</a>
-			       <p>Seeing if the auto deploy works and how long it takes.</p>
 			</div>
 		</div>
 	</div>
