6fd43951750bf419eda7a545faa73a6d8f08ee81
Andrew <ajrgraphicdesign@gmail.com>
2016-10-14 16:41:57 -0700

    Adjusting hero gradient slightly

--- site/snippets/templates.php
+++ site/snippets/templates.php
@@ -5,7 +5,7 @@
   <a href="<?php echo $email->url() ?>">
   <li>
   	<?php if($image = $email->image()): ?>
-		<img src="<?php echo $image->url() ?>" style="border:2px solid #ddd; margin-bottom:10px;">
+		<img src="<?php echo $image->url() ?>" style="border:2px solid #f1f0f0; margin-bottom:10px;">
 	<?php endif ?>
     	<small class="mono italic"><?php echo $email->title()->html() ?></small>
   </li>
