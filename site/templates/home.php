0b06eeebac65db836064c8376249abc316bf08be
Andrew <ajrgraphicdesign@gmail.com>
2016-11-04 10:57:17 -0700

    Adding a notice of change of process

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,4 +1,13 @@
 <?php snippet('header') ?>
+<div class="hero-unit flush-margin-b" style="background-color:#ffd94b;">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h6 class="subheading">While we are continuing to refine these new templates please send drafts to <a href="mailto:jsetzen@bluebeam.com">Jon Setzen</a> for a quick QA before it goes into Exact Target. This is a temporary process until we are confident we don't need to tweak the templates any more.</h6>
+			</div>
+		</div>
+	</div>
+</div>
 <div class="hero-unit hero-unit-darken" style="padding:74px;">
 	<div class="container">
 		<div class="row">
