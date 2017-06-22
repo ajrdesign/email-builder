cfee8dc6deaaeb643cfd5f679394a7a3ed3489f5
Andrew <ajrgraphicdesign@gmail.com>
2017-06-21 17:03:15 -0700

    Adding Hero Block to Basic Template

---
+++ site/snippets/sections/heroblock.php
@@ -0,0 +1,19 @@
+<table class="row responsive-hero-container">
+  <tr>
+    <td class="wrapper wrapper-flush-t-padding last">
+
+      <table class="twelve columns">
+        <tr>
+          <td class="center">
+
+            <a target="_blank" href="<?php echo $data->heroLink() ?>">
+              <img class="responsive-hero" src="<?php echo $data->heroImg() ?>" width="580" alt="<?php echo $data->heroAlt() ?>">
+            </a>
+          </td>
+        </tr>
+      </table>
+
+    </td>
+  </tr>
+
+</table>
