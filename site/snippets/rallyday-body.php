bb07c5c54c5d8e2b80739a0d44f1b4e6a108bc43
Andrew <ajrgraphicdesign@gmail.com>
2017-07-27 15:13:48 -0700

    Rally Day email changes. Added the option to remove the last image and replace it with a button instead.

--- site/snippets/rallyday-body.php
+++ site/snippets/rallyday-body.php
@@ -64,22 +64,55 @@
               </td>
             </tr>
           </table>
-          <table class="row responsive-hero-pad">
-            <tr>
-              <td class="wrapper wrapper-flush-t-padding last">
+          <?php if (!$page->heroCtaImg()->empty()): ?>
+            <table class="row responsive-hero-pad">
+              <tr>
+                <td class="wrapper wrapper-flush-t-padding last">
 
-                <table class="twelve columns">
+                  <table class="twelve columns">
+                    <tr>
+                      <td class="center">
+
+                        <a target="_blank" href="<?php echo $page->heroCtaLink() ?>">
+                          <img class="responsive-hero" src="<?php echo $page->heroCtaImg() ?>" width="580" alt="<?php echo $page->heroCtaAlt() ?>">
+                        </a>
+                      </td>
+                    </tr>
+                  </table>
+
+                </td>
+              </tr>
+
+            </table>
+            <?php else: ?>
+              <table class="row rallyday-bg">
                   <tr>
-                    <td class="center">
+                    <td class="wrapper offset-by-three last">
 
-                      <a target="_blank" href="<?php echo $page->heroCtaLink() ?>">
-                        <img class="responsive-hero" src="<?php echo $page->heroCtaImg() ?>" width="580" alt="<?php echo $page->heroCtaAlt() ?>">
-                      </a>
+                      <table class="six columns ">
+                        <tr>
+                          <td class="text-pad">
+
+                            <table class="medium-button button-blue" align="center" width="100%" border="0" cellspacing="0" cellpadding="0">
+                              <tr>
+                                <td >
+                                  <table border="0" cellspacing="0" cellpadding="0">
+                                    <tr>
+                                      <td align="center"><a target="_blank" href="<?php echo $page->heroCtaLink() ?>" target="_blank" style=""><?php echo $page->heroCtaAlt() ?></a></td>
+                                    </tr>
+                                  </table>
+                                </td>
+                              </tr>
+                            </table>
+
+
+                          </td>
+                          <td class="expander"></td>
+                        </tr>
+                      </table>
+
                     </td>
+
                   </tr>
                 </table>
-
-              </td>
-            </tr>
-
-          </table>
+          <?php endif; ?>
