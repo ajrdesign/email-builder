f6e59ab19cae6bae209ba6cd266c1cd10059152a
Max <mmurillo@bluebeam.com>
2018-06-15 16:06:31 -0700

    fixed column area

--- site/snippets/basic-body.php
+++ site/snippets/basic-body.php
@@ -7,6 +7,19 @@
 	        	<?php snippet('sections/' . $section->_fieldset(), array('data' => $section)) ?>
 	        <?php endforeach ?>
 
-	        <?php foreach($page->minorArea()->toStructure() as $minor): ?>
-	        	<?php snippet('sections/extraSection' . $minor->_fieldset(), array('data' => $minor)) ?>
-	        <?php endforeach ?>
+
+	        <?php if($page->columnAreaOne()): ?>
+
+<!-- Additional Information Section Area -->
+
+				<table class="row collapse">
+				  <tbody>
+				    <tr>
+					<?php foreach($page->columnItem()->toStructure() as $columnItem): ?>
+				      <?php snippet('sections/extraSection' . $columnItem->_fields(), array('data' => $columnItem)) ?>
+					<?php endforeach ?>
+				    </tr>
+				  </tbody>
+				</table>
+
+	        <?php endif ?>
--- site/snippets/sections/extraSection.php
+++ site/snippets/sections/extraSection.php
@@ -1,25 +1,18 @@
-  <table class="row collapse">
-  <tbody>
-    <tr>
+<!-- Additional Information Column Item -->
 
-      <?php foreach($page->minorArea()->toStructure() as $infoItem): ?>
-        <th class="6 columns">
-          <table>
-            <tr>
-              <th><a target="_blank" href="<?php echo $data->iconLink() ?>">
-                <img src="<?php echo $data->imageIcon() ?>" width="" alt="<?php echo $data->iconAlt() ?>">
-              </a></th>
-            </tr>
+  <th class="6 columns first last">
+    <table>
+      <tr>
+        <th><a target="_blank" href="<?php echo $data->iconLink() ?>">
+          <img src="<?php echo $data->imageIcon() ?>" width="" alt="<?php echo $data->iconAlt() ?>">
+        </a></th>
+      </tr>
 
-            <tr>
-            <td class="">
-              <a href="<?= $data->entryLink() ?>"><?= $data->entryTitle() ?></a>
-            </td>
-          </table>
-        </th>
-      <?php endforeach ?>
+      <tr>
+      <td class="">
+        <a href="<?= $data->entryLink() ?>"><?= $data->entryTitle() ?></a>
+      </td>
+    </table>
+  </th>
 
-
-    </tr>
-  </tbody>
-</table>
+<!-- End ofAdditional Information Column Item -->
