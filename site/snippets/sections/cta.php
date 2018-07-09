fa24963f52b33ac9dad934f7a60e56710d36f0e4
Max <mmurillo@bluebeam.com>
2018-07-09 03:06:19 -0700

    updated cta button

--- site/snippets/sections/cta.php
+++ site/snippets/sections/cta.php
@@ -3,7 +3,7 @@
 
   <table class="row">
       <tr>
-        <td class="wrapper last">
+        <td>
           <table class="large-6 small-6 columns ">
             <tr>
               <td class="text-pad center">
@@ -13,16 +13,19 @@
                       <table border="0" cellspacing="0" cellpadding="0">
                         <tr>
                           <td align="center">
+                           <div>
                             <!--[if mso]>
-                              <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="<?= $data->url() ?>?src=<?php ecco(!$page->tracking()->isEmpty(), $page->tracking()) ?>" style="height:45px;v-text-anchor:middle;width:200px;" arcsize="0%" stroke="f" fillcolor="#8bc34a">
+                              <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="<?= $data->url() ?>?src=<?php ecco(!$page->tracking()->isEmpty(), $page->tracking()) ?>" style="height:35px;v-text-anchor:middle;width:200px;" arcsize="9%" stroke="f" fillcolor="#6BBD45">
                                 <w:anchorlock/>
                                 <center>
                               <![endif]-->
-                                  <a target="_blank" href="<?= $data->url() ?>?src=<?php ecco(!$page->tracking()->isEmpty(), $page->tracking()) ?>" target="_blank" style="color:#ffffff;"><?= $data->text() ?></a>
+                                  <a href="<?= $data->url() ?>?src=<?php ecco(!$page->tracking()->isEmpty(), $page->tracking()) ?>"
+                            style="background-color:#6BBD45;border-radius:3px;color:#ffffff;display:inline-block;font-family:sans-serif;font-size:16px;font-weight:bold;line-height:1em;text-align:center;text-decoration:none;width:auto;padding: 16px;-webkit-text-size-adjust:none;"><?= $data->text() ?></a>
                               <!--[if mso]>
                                 </center>
                               </v:roundrect>
                             <![endif]-->
+                          </div>
                           </td>
                         </tr>
                       </table>
@@ -36,4 +39,3 @@
   </table>
 
 <!-- End of CTA Button -->
-
